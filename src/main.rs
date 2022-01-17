48078754265e5cb3e40e6791b4217a78dda0ca63
Jesse Hallett <jesse@sitr.us>
2022-01-17 16:56:02 -0500

    we have a cute little warp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
+use async_graphql_warp::{GraphQLBadRequest, GraphQLResponse};
+use http::StatusCode;
+use std::convert::Infallible;
+use warp::Rejection;
+use warp::{http::Response as HttpResponse, Filter};
+
+struct Query;
+
+#[Object]
+impl Query {
+    /// Returns the sum of a and b
+    async fn add(&self, a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+
+    println!("Playground: http://localhost:8000");
+
+    let graphql_post = async_graphql_warp::graphql(schema).and_then(
+        |(schema, request): (
+            Schema<Query, EmptyMutation, EmptySubscription>,
+            async_graphql::Request,
+        )| async move {
+            Ok::<_, Infallible>(GraphQLResponse::from(schema.execute(request).await))
+        },
+    );
+
+    let graphql_playground = warp::path::end().and(warp::get()).map(|| {
+        HttpResponse::builder()
+            .header("content-type", "text/html")
+            .body(playground_source(GraphQLPlaygroundConfig::new("/")))
+    });
+
+    let routes = graphql_playground
+        .or(graphql_post)
+        .recover(|err: Rejection| async move {
+            if let Some(GraphQLBadRequest(err)) = err.find() {
+                return Ok::<_, Infallible>(warp::reply::with_status(
+                    err.to_string(),
+                    StatusCode::BAD_REQUEST,
+                ));
+            }
+
+            Ok(warp::reply::with_status(
+                "INTERNAL_SERVER_ERROR".to_string(),
+                StatusCode::INTERNAL_SERVER_ERROR,
+            ))
+        });
+
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
