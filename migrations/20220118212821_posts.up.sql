67a1b98dee22ef80dc509fb01184b7efea14d90f
Jesse Hallett <jesse@sitr.us>
2022-01-23 17:09:59 -0500

    initial migration

--- migrations/20220118212821_posts.up.sql
+++ migrations/20220118212821_posts.up.sql
@@ -1,1 +1,10 @@
--- Add up migration script here
+create table users (
+    id serial primary key,
+    username text not null
+);
+
+create table posts (
+    id serial primary key,
+    author_id integer references users(id) not null,
+    content text
+);
