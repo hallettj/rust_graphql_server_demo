0e49b5c414768989383b5226f8fa6573672cc43f
Jesse Hallett <jesse@sitr.us>
2022-01-23 16:52:01 -0500

    fix port mapping for dockerized database

--- db_create.sh
+++ db_create.sh
@@ -5,6 +5,6 @@ set -e
 docker run \
     --name graphql_server_demo_db \
     -e POSTGRES_PASSWORD=password \
-    -p 127.0.0.1:5432:44544/tcp \
+    -p 127.0.0.1:44544:5432/tcp \
     --detach \
     postgres
