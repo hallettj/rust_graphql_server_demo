b00f5e6e851e45a05c7e3a4a4c5169c0ffe5af22
Jesse Hallett <jesse@sitr.us>
2022-01-24 17:15:12 -0500

    mutations to create and delete posts

--- src/api/mutation.rs
+++ src/api/mutation.rs
@@ -1,6 +1,6 @@
 use async_graphql::{Context, Object, Result};
 
-use super::user::User;
+use super::{post::Post, user::User};
 use crate::db::get_db_from_ctx;
 
 pub struct Mutation;
@@ -8,20 +8,52 @@ pub struct Mutation;
 #[Object]
 impl Mutation {
     async fn create_user(&self, ctx: &Context<'_>, username: String) -> Result<User> {
-        let db = get_db_from_ctx(ctx)?;
-        let user = sqlx::query_as!(User, "
+        let user = sqlx::query_as!(
+            User,
+            "
             insert into users (username)
             values ($1)
             returning id, username
-        ", username).fetch_one(db).await?;
+            ",
+            username
+        )
+        .fetch_one(get_db_from_ctx(ctx)?)
+        .await?;
         Ok(user)
     }
 
     async fn delete_user(&self, ctx: &Context<'_>, id: i32) -> Result<bool> {
-        let db = get_db_from_ctx(ctx)?;
-        sqlx::query!("
-            delete from users where id = $1
-        ", id).execute(db).await?;
+        sqlx::query!("delete from users where id = $1", id)
+            .execute(get_db_from_ctx(ctx)?)
+            .await?;
+        Ok(true)
+    }
+
+    async fn create_post(
+        &self,
+        ctx: &Context<'_>,
+        author_id: i32,
+        content: String,
+    ) -> Result<Post> {
+        let post = sqlx::query_as!(
+            Post,
+            "
+            insert into posts (author_id, content)
+            values ($1, $2)
+            returning id, author_id, content
+            ",
+            author_id,
+            content,
+        )
+        .fetch_one(get_db_from_ctx(ctx)?)
+        .await?;
+        Ok(post)
+    }
+
+    async fn delete_post(&self, ctx: &Context<'_>, id: i32) -> Result<bool> {
+        sqlx::query!("delete from posts where id = $1", id)
+            .execute(get_db_from_ctx(ctx)?)
+            .await?;
         Ok(true)
     }
 }
