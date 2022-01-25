5ac5d82f49521ff1a8e2161907b548ffeefc8959
Jesse Hallett <jesse@sitr.us>
2022-01-25 15:02:30 -0500

    add `posts` field to `User`

--- src/api/user.rs
+++ src/api/user.rs
@@ -1,7 +1,29 @@
-use async_graphql::SimpleObject;
+use async_graphql::{ComplexObject, Context, Result, SimpleObject};
+
+use crate::db::get_db_from_ctx;
+
+use super::post::Post;
 
 #[derive(Debug, SimpleObject)]
+#[graphql(complex)]
 pub struct User {
     pub id: i32,
     pub username: String,
+}
+
+#[ComplexObject]
+impl User {
+    async fn posts(&self, ctx: &Context<'_>) -> Result<Vec<Post>> {
+        let posts = sqlx::query_as!(
+            Post,
+            "
+            select id, author_id, content from posts
+            where author_id = $1
+            ",
+            self.id,
+        )
+        .fetch_all(get_db_from_ctx(ctx)?)
+        .await?;
+        Ok(posts)
+    }
 }
